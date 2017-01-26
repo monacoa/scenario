457480b236974b066bdf5de483da796953b07a89
andrea monaco <andrea.monaco@prometeia.com>
2017-01-26 12:41:06 +0100

    secondo commit

--- sc_elab/excel_hook/interfacce_base.py
+++ sc_elab/excel_hook/interfacce_base.py
@@ -17,11 +17,47 @@ def win32com_menu_test(control):
     xlcAlert("Some text has been written to the current cell")
 
 
+"""
+#@xl_func
+def checkbox_example(control):
+    xl = xl_app()
+    check_box = xl.ActiveSheet.CheckBoxes(xl.Caller)
+    if check_box.Value == 1:
+        xl.Range("checkbox_output").Value = "CHECKED"
+    else:
+        xl.Range("checkbox_output").Value = "Click the check box"
+"""
+
+
 @xl_func
 def button_example(control):
-    xl = xl_app()
-    range = xl.Range("PLUTO")
+
+    xl          = xl_app()
+    range       = xl.Range("PLUTO")
+    range_p     = xl.Range("PIPPO")
+
+
     range.Value = range.Value + 1
+    
+    #range.Interior.ColorIndex = 6
+    #range_p.Interior.ColorIndex = 6
+    range_p.Style = "Normal"
+    
+    range_p.Style.Font.Name = "Verdana"
+    range_p.Style.Font.Size = 16
+    range_p.Style.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red);
+
+    #range_p.Style.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Red)
+    #range_p.Style.Interior.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Gray)
+    #range_p.Style.Interior.Pattern = Excel.XlPattern.xlPatternSolid
+
+    
+    #ws = wb.Worksheets("Sheet1")
+    """
+    for i in range (1,21):
+        xl.Cells(i,1).Value = i
+        #xl.Cells(i,1).Interior.ColorIndex = i
+    """
 
 
 @xl_func
@@ -44,8 +80,22 @@ def sommaro(a= 1, b = 2):
     
     return a + 2*b
 
+    """
+    import win32com.client as win32
+    excel = win32.gencache.EnsureDispatch('Excel.Application')
+    wb = excel.Workbooks.Add()
+    ws = wb.Worksheets("Sheet1")
+    for i in range (1,21):
+        ws.Cells(i,1).Value = i
+        ws.Cells(i,1).Interior.ColorIndex = i
+    wb.SaveAs('cell_color.xlsx')
+    excel.Application.Quit()
+    """
 
 
+
+# Connection example: Windows, without a DSN, using the Windows SQL Server driver
+# Connection example: Windows, without a DSN, using the Windows SQL Server driver
 #def on_text_button(a = 1):
     
 #    return 'pippo'
